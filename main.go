ea6d4d729cd1141455b481d804db5f0dd932d395
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-06-19 15:59:18 +0530

    repos CRUD functions has been created

--- main.go
+++ main.go
@@ -89,33 +89,84 @@ func DeleteUser(db *sql.DB, id string) error {
 }
 
 type Repo struct {
-	Id         string    `json:"id,omitempty"`
-	Name       string    `json:"name,omitempty"`
-	UserId     string    `json:"user_id,omitempty"`
-	IssueCount int       `json:"issue_count,omitempty"`
-	CreatedAt  time.Time `json:"created_at,omitempty"`
-	UpdatedAt  time.Time `json:"updated_at,omitempty"`
+	ID          string    `json:"id,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	UserId      string    `json:"user_id,omitempty"`
+	IssuesCount int       `json:"issue_count,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
 }
 
-func GetRepo(db *sql.DB, id string) (repo, error) {
+func GetRepo(db *sql.DB, id string) (Repo, error) {
 
 	var name, userId, createdAt, updatedAt string
 	var issueCount int
 
-	row := db.QueryRow("SELECT name, user_id,issueCount,created_at, updated_at FROM repos WHERE id=$1", id)
+	row := db.QueryRow("SELECT name, user_id,issue_count,created_at, updated_at FROM repos WHERE id=$1", id)
 	err := row.Scan(&name, &userId, &issueCount, &createdAt, &updatedAt)
 	if err != nil {
-		return User{}, err
+		return Repo{}, err
 	}
 
-	repo := Repos{
+	CreatedAt, err := time.Parse(time.RFC3339, createdAt)
+
+	if err != nil {
+		return Repo{}, err
+	}
+
+	UpdatedAt, err := time.Parse(time.RFC3339, updatedAt)
+
+	if err != nil {
+		return Repo{}, err
+	}
+
+	repo := Repo{
 		Name:        name,
-		UserId:      user_id,
-		IssuesCount: issue_count,
-		CreatedAt:   created_at,
-		UpdatedAt:   updated_at,
+		UserId:      userId,
+		IssuesCount: issueCount,
+		CreatedAt:   CreatedAt,
+		UpdatedAt:   UpdatedAt,
 	}
 
+	return repo, nil
+}
+
+func CreateRepo(db *sql.DB, repo Repo) error {
+
+	_, err := db.Exec(`INSERT INTO repos(id,name, user_id, issue_count, created_at, updated_at)
+						VALUES($1,$2,$3,$4,$5,$6)`,
+		repo.ID,
+		repo.Name,
+		repo.UserId,
+		repo.IssuesCount,
+		repo.CreatedAt.Format(time.RFC3339),
+		repo.UpdatedAt.Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateRepo(db *sql.DB, repo Repo) error {
+
+	_, err := db.Exec(`UPDATE repos SET name = $1,user_id = $2,issue_count =$3, updated_at = $4 WHERE id = $5`,
+		repo.Name, repo.UserId, repo.IssuesCount, time.Now().Format(time.RFC3339), repo.ID)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteRepo(db *sql.DB, id string) error {
+
+	_, err := db.Exec(`DELETE FROM repos WHERE id = $1`, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
@@ -136,6 +187,8 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	//CRUD functions for user under this statement
 
 	// user, err := GetUser(db, "ac6f8b68-8f31-48ea-a436-05b9813b484b")
 	// if err != nil {
@@ -179,5 +232,52 @@ func main() {
 	// 	return
 	// }
 	// fmt.Println("deleted issue")
+
+	// CRUD functions for repos under this statement
+
+	// repo, err := GetRepo(db, "d360c6f3-60dc-4846-bb6a-0919a1817d5e")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println(repo)
+
+	// err = CreateRepo(db, Repo{
+	// 	ID:          "d360c6f3-60dc-4846-bb6a-0919a1817d5e",
+	// 	Name:        "sridhar",
+	// 	UserId:      "ac6f8b68-8f31-48ea-a436-05b9813b484b",
+	// 	IssuesCount: 1,
+	// 	CreatedAt:   time.Now(),
+	// 	UpdatedAt:   time.Now()})
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println("created an Repo")
+
+	// err = UpdateRepo(db, Repo{
+	// 	ID:          "d360c6f3-60dc-4846-bb6a-0919a1817d5e",
+	// 	Name:        "vramana",
+	// 	UserId:      "ac6f8b68-8f31-48ea-a436-05b9813b484b",
+	// 	IssuesCount: 10,
+	// 	// CreatedAt: time.Now()
+	// 	UpdatedAt: time.Now()})
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println("updated repo")
+
+	// err = DeleteRepo(db, "d360c6f3-60dc-4846-bb6a-0919a1817d5e")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println("deleted repo")
+
+	//CRUD functions for issues under this section
 
 }
