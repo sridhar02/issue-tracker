60210ed8845a02691e90a6a83242d40416477b8d
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-06-20 14:07:41 +0530

    migration 2 changes 3 unique constraints are added

--- migrations/002.down.sql
+++ migrations/002.down.sql
@@ -1,3 +1,6 @@
 ALTER TABLE users DROP CONSTRAINT unique_email_address;
 
+ALTER TABLE users DROP CONSTRAINT unique_username;
+
+
 ALTER TABLE repos DROP CONSTRAINT unique_user_id;
--- migrations/002.up.sql
+++ migrations/002.up.sql
@@ -1,6 +1,5 @@
 ALTER TABLE users ADD CONSTRAINT unique_email_address UNIQUE(email);
 
-ALTER TABLE users ADD CONSTRAINT unique_name UNIQUE(username);
-
+ALTER TABLE users ADD CONSTRAINT unique_username UNIQUE(username);
 
 ALTER TABLE repos ADD CONSTRAINT unique_user_id UNIQUE(user_id,name);
