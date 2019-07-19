89e1a8e8862ba531a2cef1238760b782dbd11bb4
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-07-19 16:19:01 +0530

    collaborator issue checks of close,open,pin,unpin,lock &unlock

--- cypress/integration/issue.spec.js
+++ cypress/integration/issue.spec.js
@@ -1,6 +1,4 @@
-describe("new issue and issue comment", () =>{
-
-
+describe("issue-tests repo owner", () =>{
 	beforeEach(() => {
 		cy.visit("/login")
 		cy.get('[name="username"]').type("Sridhar1997")
@@ -26,7 +24,7 @@ describe("new issue and issue comment", () =>{
 		cy.get('.comment').click()
 	})
 
-	it.only("comment-close", () =>{
+	it("comment-close", () =>{
 		cy.visit("/Sridhar1997/Test/issues/7")
 		cy.get('[name="comment_and_close"]').click()
 	})
@@ -42,7 +40,7 @@ describe("new issue and issue comment", () =>{
 		cy.get('[name="pin-issue"]').click()
 	})
 
-	it("issue-pin", () =>{
+	it("issue-unpin", () =>{
 		cy.visit("/Sridhar1997/Test/issues/7")
 		cy.get('[name="unpin-issue"]').click()
 	})
@@ -56,9 +54,44 @@ describe("new issue and issue comment", () =>{
 		cy.visit("/Sridhar1997/Test/issues/7")
 		cy.get('[name="unlock-button"]').click()
 	})
+})
 
 
+describe("issue-tests collaborators",() =>{
+	beforeEach(() => {
+		cy.visit("/login")
+		cy.get('[name="username"]').type("DONE456")
+		cy.get('[name="password"]').type("x49b9LgbxdU7g2B")
+		cy.get('button').click();
+	})
 
+	it("collaborator-close",() =>{
+		cy.visit("/Sridhar1997/funny/issues/1")
+		cy.get('[name="comment_and_close"]').click()	
+	})
 
+	it("collaborator-open",() =>{
+		cy.visit("/Sridhar1997/funny/issues/1")
+		cy.get('[name="comment_and_open"]').click()	
+	})
 
+	it("collaborator-pin", () =>{
+		cy.visit("/Sridhar1997/funny/issues/1")
+		cy.get('[name="pin-issue"]').click()
+	})
+
+	it("collaborator-unpin", () =>{
+		cy.visit("/Sridhar1997/funny/issues/1")
+		cy.get('[name="unpin-issue"]').click()
+	})
+
+	it("collaborator-lock",() => {
+		cy.visit("/Sridhar1997/funny/issues/1")
+		cy.get('[name="lock-button"]').click()
+	})
+
+	it("collaborator-unlock",() => {
+		cy.visit("/Sridhar1997/funny/issues/1")
+		cy.get('[name="unlock-button"]').click()
+	})
 })
