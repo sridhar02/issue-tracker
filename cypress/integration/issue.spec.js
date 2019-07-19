efd8f87e5bb51712322027d04b570ffdbb8e7758
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-07-19 15:54:32 +0530

    comment properties such as open,close,pin,unpin,lock and unlock are tested and buttons are given names according to there action

--- cypress/integration/issue.spec.js
+++ cypress/integration/issue.spec.js
@@ -20,13 +20,42 @@ describe("new issue and issue comment", () =>{
 		cy.get('[name="comment_and_close"]').click()
 	})
 
-	it.only("new-comment", () =>{
+	it("new-comment", () =>{
 		cy.visit("/Sridhar1997/Test/issues/7")
 		cy.get('[name="body"]').type("elven loves mike")
 		cy.get('.comment').click()
 	})
 
+	it.only("comment-close", () =>{
+		cy.visit("/Sridhar1997/Test/issues/7")
+		cy.get('[name="comment_and_close"]').click()
+	})
 
+	it("comment-open", () =>{
+		cy.visit("/Sridhar1997/Test/issues/7")
+		cy.get('[name="comment_and_open"]').click()
+	})
+
+
+	it("issue-pin", () =>{
+		cy.visit("/Sridhar1997/Test/issues/7")
+		cy.get('[name="pin-issue"]').click()
+	})
+
+	it("issue-pin", () =>{
+		cy.visit("/Sridhar1997/Test/issues/7")
+		cy.get('[name="unpin-issue"]').click()
+	})
+
+	it("issue-lock",() => {
+		cy.visit("/Sridhar1997/Test/issues/7")
+		cy.get('[name="lock-button"]').click()
+	})
+
+	it("issue-unlock",() => {
+		cy.visit("/Sridhar1997/Test/issues/7")
+		cy.get('[name="unlock-button"]').click()
+	})
 
 
 
