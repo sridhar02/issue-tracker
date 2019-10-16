072146558acc82c91b4835152a57fcd164ebaabc
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-10-16 13:35:53 +0530

    User page has been created.

---
+++ issue_hub/pages/user.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+
+import Router from "next/router";
+
+import Link from "next/link";
+
+import axios from "axios";
+
+import { withStyles } from "@material-ui/core/styles";
+
+import { Button, Typography } from "@material-ui/core";
+
+import TextField from "@material-ui/core/TextField";
+
+import cx from "classnames";
+
+class _User extends Component {
+  render() {
+    return <div></div>;
+  }
+}
