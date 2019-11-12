c93f77ee6c658f857e25c32b67247a01c06a3fe4
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-11-12 20:17:30 +0530

    Popper is written and sign out is implemented in popper menu .

--- issue_hub/utils/utils.js
+++ issue_hub/utils/utils.js
@@ -15,6 +15,9 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import Popper from "@material-ui/core/Popper";
+import Fade from "@material-ui/core/Fade";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
 
 const menuStyles = theme => ({
   dashboard: {
@@ -115,6 +118,22 @@ const navbarStyles = theme => ({
     padding: 0,
     margin: 0,
     border: 0
+  },
+  popper: {
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column"
+  },
+  usernamePopper: {
+    padding: theme.spacing(1),
+    borderBottom: "1px solid #ddd"
+  },
+  typography: {
+    padding: theme.spacing(1)
+  },
+  yourGists: {
+    padding: theme.spacing(1),
+    borderBottom: "1px solid #ddd"
   }
 });
 
@@ -123,7 +142,9 @@ class _Navbar extends Component {
     super(props);
     this.state = {
       navbar: false,
-      user: undefined
+      user: undefined,
+      anchorEl: null,
+      open: false
     };
   }
 
@@ -148,13 +169,25 @@ class _Navbar extends Component {
     });
   };
 
+  handleClickPoper = event => {
+    const { currentTarget } = event;
+    this.setState(state => ({
+      anchorEl: currentTarget,
+      open: !state.open
+    }));
+  };
+
+  handleSignout = () => {
+    localStorage.removeItem("secret");
+    Router.push("/login");
+  };
   render() {
     const { classes } = this.props;
     const { navbar, user } = this.state;
     const menu = navbar ? <Menu user={user} /> : <div></div>;
-    if (user === undefined) {
-      return null;
-    }
+    const { anchorEl, open } = this.state;
+    const id = open ? "simple-popper" : null;
+
     return (
       <AppBar position="static" className={classes.container}>
         <div className={cx(classes.container, "container")}>
@@ -168,7 +201,11 @@ class _Navbar extends Component {
                 >
                   <MenuIcon />
                 </Button>
-                <GitHubIcon />
+                {user && (
+                  <Link href={`/user/${user.username}`}>
+                    <GitHubIcon />
+                  </Link>
+                )}
                 <TextField
                   className={cx(classes.textField, "d-none d-md-block")}
                 />
@@ -178,17 +215,54 @@ class _Navbar extends Component {
                 <div className="d-none d-md-block">Explore</div>
                 <div className={cx(classes.space, "d-none d-md-block")}></div>
                 <NotificationsIcon />
-                <div className="d-none d-md-block">
-                  <img src={user.image} className={classes.userImage} />
-                  <button type="button" className={classes.dropDown}>
-                    <ArrowDropDownIcon onClick="" />
-                  </button>
-                  {/*<Popper id={id} open={open} anchorEl={anchorEl}>
-                    <div className={classes.paper}>
-                      The content of the Popper.
-                    </div>
-                  </Popper>*/}
-                </div>
+                {user && (
+                  <div className="d-none d-md-block">
+                    <button
+                      type="button"
+                      aria-describedby={id}
+                      onClick={this.handleClickPoper}
+                      className={classes.dropDown}
+                    >
+                      <img src={user.image} className={classes.userImage} />
+                      <ArrowDropDownIcon />
+                    </button>
+                    <Popper id={id} open={open} anchorEl={anchorEl}>
+                      <Paper className={classes.popper}>
+                        <Typography className={classes.usernamePopper}>
+                          signed as {user.username}
+                        </Typography>
+                        <Button className={classes.typography}>
+                          Your Profile
+                        </Button>
+                        <Button className={classes.typography}>
+                          Your Repositories
+                        </Button>
+                        <Button className={classes.typography}>
+                          Your Projects
+                        </Button>
+                        <Button className={classes.typography}>
+                          Your stars
+                        </Button>
+                        <Button className={classes.yourGists}>
+                          Your gists
+                        </Button>
+                        <Button className={classes.typography}>
+                          Feature Preview
+                        </Button>
+                        <Button className={classes.typography}>Help</Button>
+                        <Button className={classes.typography}>Settings</Button>
+                        <Link href="/login">
+                          <Button
+                            className={classes.typography}
+                            onClick={this.handleSignout}
+                          >
+                            sign out
+                          </Button>
+                        </Link>
+                      </Paper>
+                    </Popper>
+                  </div>
+                )}
               </div>
               {menu}
               <div className={cx(classes.overview, "d-md-none")}>
@@ -203,6 +277,10 @@ class _Navbar extends Component {
     );
   }
 }
+
+_Navbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 export const Navbar = withStyles(navbarStyles)(_Navbar);
 
