c86ac8068b5f47ee427a09a7628a8bd57e60c542
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-11-13 13:38:20 +0530

    sign up UI is made similar to github.

--- issue_hub/pages/join.js
+++ issue_hub/pages/join.js
@@ -1,20 +1,90 @@
 import React, { Component, Fragment } from "react";
-
 import Router from "next/router";
-
 import Link from "next/link";
-
-import TextField from "@material-ui/core/TextField";
-
-import { withStyles } from "@material-ui/core/styles";
-
-import { Button, Typography } from "@material-ui/core";
-
 import cx from "classnames";
-
 import axios from "axios";
 
-class Signup extends Component {
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import { Button, Typography } from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+const navbarStyles = theme => ({
+  navbar: {
+    display: "flex",
+    backgroundColor: "black",
+    justifyContent: "space-between",
+    color: "#fff",
+    padding: theme.spacing(2)
+  },
+  signup: {
+    color: "#fff",
+    border: "1px solid #fff"
+  },
+  signin: {
+    color: "#fff"
+  },
+  searchbar: {
+    backgroundColor: "#fff",
+    borderRadius: "4px",
+    margin: theme.spacing(1),
+    height: "30px"
+  }
+});
+
+function _Navbar({ classes }) {
+  return (
+    <div className={cx(classes.navbar, "d-none d-md-flex")}>
+      <GitHubIcon />
+      <div>Why Github</div>
+      <div>Enterprise</div>
+      <div>Explore</div>
+      <div>Marketplace</div>
+      <div>Pricing</div>
+      <div></div>
+      <TextField variant="outlined" className={classes.searchbar} />
+      <Link href="/login">
+        <Button className={classes.signin}>Sign in</Button>
+      </Link>
+      <Button className={classes.signup}>Sign up</Button>
+    </div>
+  );
+}
+const Navbar = withStyles(navbarStyles)(_Navbar);
+
+const signupStyles = theme => ({
+  container: {
+    backgroundColor: "#2b3137",
+    color: "#fff",
+    padding: "20px",
+    margin: 0,
+    [theme.breakpoints.up("md")]: {
+      padding: "100px",
+      paddingRight: "150px",
+      maxWidth: "1368px",
+      margin: 0,
+      display: "flex"
+    }
+  },
+  signup: {
+    backgroundColor: "#28A745",
+    margin: "20px 0px 0px",
+    padding: "25px",
+    color: "#fff",
+    width: "100%"
+  },
+  sidebar: {
+    borderRadius: "4px",
+    padding: theme.spacing(2),
+    backgroundColor: "#fff",
+    color: "black",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  }
+});
+
+class _Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -47,58 +117,80 @@ class Signup extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="row">
-            <div className="col-12">
-              <Typography variant="h6">Name</Typography>
-              <TextField
-                type="text"
-                name="name"
-                margin="normal"
-                variant="outlined"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-              <Typography variant="h6">Username</Typography>
-              <TextField
-                type="text"
-                name="username"
-                margin="normal"
-                variant="outlined"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-              <Typography variant="h6">Email</Typography>
-              <TextField
-                type="text"
-                name="email"
-                margin="normal"
-                variant="outlined"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-              <Typography variant="h6">Password</Typography>
-              <TextField
-                type="password"
-                name="password"
-                margin="normal"
-                variant="outlined"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-              <div>
-                <Button variant="contained" color="primary" type="submit">
-                  Signup
-                </Button>
+      <Fragment>
+        <Navbar />
+        <div className={cx(classes.container, "container")}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="col-lg-7 d-none d-md-block">
+                <Typography variant="h2">Built for developers</Typography>
+                <Typography variant="h6">
+                  GitHub is a development platform inspired by the way you work.
+                  From open source to business, you can host and review code,
+                  manage projects, and build software alongside 40 million
+                  developers.
+                </Typography>
+              </div>
+              <div className={cx(classes.sidebar, "col-lg-5")}>
+                <div>
+                  <Typography variant="h5">Create your account</Typography>
+                </div>
+                <Typography variant="body2">Name</Typography>
+                <TextField
+                  type="text"
+                  name="name"
+                  margin="normal"
+                  variant="outlined"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+                <Typography variant="body2">Username</Typography>
+                <TextField
+                  type="text"
+                  name="username"
+                  margin="normal"
+                  variant="outlined"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+                <Typography variant="body2">Email</Typography>
+                <TextField
+                  type="text"
+                  name="email"
+                  margin="normal"
+                  variant="outlined"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+                <Typography variant="body2">Password</Typography>
+                <TextField
+                  type="password"
+                  name="password"
+                  margin="normal"
+                  variant="outlined"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+                <div>
+                  <Button
+                    variant="contained"
+                    className={classes.signup}
+                    type="submit"
+                  >
+                    Sign up for Github
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </Fragment>
     );
   }
 }
+
+const Signup = withStyles(signupStyles)(_Signup);
 
 export default Signup;
